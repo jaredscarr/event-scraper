359652710f5bae7fa40e83768754c1e5c228e214
Jared Scarr <jaredscarr@pm.me>
2024-05-17 13:01:29 -0700

    Successfully parses html and creates events that are stored in a vec

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,60 @@
+use std::ops::Add;
+use scraper::{Html, Selector, ElementRef};
 use lambda_http::{run, service_fn, tracing, Body, Error, Request, RequestExt, Response};
+use scraper::html::Select;
 
-/// This is the main body for the function.
-/// Write your code inside it.
+// This is the main body for the function.
+// Write your code inside it.
+
+#[derive(Clone, Debug)]
+struct Event {
+    date: String,
+    headliner: String,
+    support_talent: String,
+    showtime: String,
+    venue: String,
+    age: String,
+}
+
+fn get_string_from_selector(selector: String, document: &ElementRef) -> String {
+    let selector = Selector::parse(&selector).unwrap();
+    let option = document.select(&selector).next().map(|element|element.text().collect::<String>().trim().to_owned());
+    option.unwrap_or_else(|| String::from(""))
+}
+
 async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
-    // create a handler that will scrape based on the date. If the date hasn't changed don't run again
+    // TODO: create a handler that will scrape based on the date. If the date hasn't changed don't run again
     let response = reqwest::get(
         "https://elcorazonseattle.com/",
     )
         .await?
         .text()
         .await?;
-    // println!("body = {response:?}");
-    let document = scraper::Html::parse_document(&response);
-    println!("{document:?}");
 
+    // grap one selector to get how many events are there
+    let document = Html::parse_document(&response);
+    let event_content = Selector::parse("div.event-info-block").unwrap();
+    let mut event_vec: Vec<Event> = Vec::new();
 
+    for event in document.select(&event_content) {
+        let date = get_string_from_selector(String::from("p.event-date"), &event);
+        let headliner = get_string_from_selector(String::from("p.headliners"), &event);
+        let support_talent = get_string_from_selector(String::from("p.supporting-talent"), &event);
+        let showtime = get_string_from_selector(String::from("p.doortime-showtime"), &event);
+        let venue = get_string_from_selector(String::from("p.venue"), &event);
+        let age = get_string_from_selector(String::from("span.ages"), &event);
+
+        let new_event = Event {
+            date,
+            headliner,
+            support_talent,
+            showtime,
+            venue,
+            age,
+        };
+        event_vec.push(new_event);
+    }
+    println!("{event_vec:?}");
 
     // Extract some useful information from the request
     let who = event
@@ -27,7 +67,7 @@ async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
     // It will be serialized to the right response event automatically by the runtime
     let resp = Response::builder()
         .status(200)
-        .header("content-type", "text/html")
+        .header("content-type", "text/html")// TODO: switch to json
         .body(message.into())
         .map_err(Box::new)?;
     Ok(resp)
