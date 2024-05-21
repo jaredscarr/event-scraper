022a5af3fe9b38638b583164b4a86a2d75dfb4bd
Jared Scarr <jaredscarr@pm.me>
2024-05-21 14:13:44 -0700

    url links added to Event struct.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use scraper::{Html, Selector, ElementRef};
 use lambda_http::{run, service_fn, tracing, Body, Error, Request, Response};
+use scraper::{ElementRef, Html, Selector};
 use serde::Serialize;
-use serde_json;
-
 
 // This is the main body for the function.
 // Write your code inside it.
@@ -11,6 +9,7 @@ use serde_json;
 struct Event {
     date: String,
     headliner: String,
+    url: String,
     support_talent: String,
     showtime: String,
     venue: String,
@@ -19,35 +18,45 @@ struct Event {
 
 fn get_string_from_selector(selector: String, document: &ElementRef) -> String {
     let selector = Selector::parse(&selector).unwrap();
-    let option = document.select(&selector).next().map(|element|element.text().collect::<String>().trim().to_owned());
-    option.unwrap_or_else(|| String::from(""))
+    let option = document
+        .select(&selector)
+        .next()
+        .map(|element| element.text().collect::<String>().trim().to_owned());
+    option.unwrap_or_else(|| "".into())
+}
+
+fn get_string_from_attr(selector: String, document: &ElementRef, attribute: String) -> String {
+    let selector = Selector::parse(&selector).unwrap();
+    let option = document.select(&selector).next().unwrap().value().attr(&attribute);
+    option.unwrap_or_else(|| "".into()).to_owned()
 }
 
 async fn function_handler(_event: Request) -> Result<Response<Body>, Error> {
-    // TODO: create a handler that will scrape based on the date. If the date hasn't changed don't run again
-    let response = reqwest::get(
-        "https://elcorazonseattle.com/",
-    )
+    // println!("{_event:?}");
+    let response = reqwest::get("https://elcorazonseattle.com/")
         .await?
         .text()
         .await?;
 
     // grap one selector to get how many events are there
     let document = Html::parse_document(&response);
-    let event_content = Selector::parse("div.event-info-block").unwrap();
+    // let event_content = Selector::parse("div.event-info-block").unwrap();
+    let event_content = Selector::parse("div.seetickets-list-event-container").unwrap();
     let mut event_vec: Vec<Event> = Vec::new();
 
     for event in document.select(&event_content) {
-        let date = get_string_from_selector(String::from("p.event-date"), &event);
-        let headliner = get_string_from_selector(String::from("p.headliners"), &event);
-        let support_talent = get_string_from_selector(String::from("p.supporting-talent"), &event);
-        let showtime = get_string_from_selector(String::from("p.doortime-showtime"), &event);
-        let venue = get_string_from_selector(String::from("p.venue"), &event);
-        let age = get_string_from_selector(String::from("span.ages"), &event);
+        let date = get_string_from_selector("p.event-date".into(), &event);
+        let headliner = get_string_from_selector("p.headliners".into(), &event);
+        let url = get_string_from_attr("p.event-title > a".into(), &event, "href".into());
+        let support_talent = get_string_from_selector("p.supporting-talent".into(), &event);
+        let showtime = get_string_from_selector("p.doortime-showtime".into(), &event);
+        let venue = get_string_from_selector("p.venue".into(), &event);
+        let age = get_string_from_selector("span.ages".into(), &event);
 
         let new_event = Event {
             date,
             headliner,
+            url,
             support_talent,
             showtime,
             venue,
@@ -55,6 +64,7 @@ async fn function_handler(_event: Request) -> Result<Response<Body>, Error> {
         };
         event_vec.push(new_event);
     }
+
 
     let json_message = serde_json::to_string(&event_vec).unwrap();
     // Return something that implements IntoResponse.
